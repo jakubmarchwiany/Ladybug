8f058da7845b366f177bae089ab78f92f5a463f6
kvbik <50952542+xkvbikx@users.noreply.github.com>
2020-02-21 19:25:43 +0100

    changes v5

--- venv/MainFrame.py
+++ venv/MainFrame.py
@@ -1,13 +1,25 @@
-#Glowne Okno Aplikacji
+# Glowne Okno Aplikacji
 
-import pygame
+import pygame,sys,os
 
 pygame.init()
-pygame.display.set_caption("Biedronkowa Zawierucha")
+pygame.display.set_caption( "Biedronkowa Zawierucha" )
 
-height=500
-weight=500
+height = 500
+widght = 500
 
-screen=pygame.display.set_mode((height,weight))
+screen = pygame.display.set_mode((height, weight))
+
+
+
+
+show = 0
+
+while True:
+    if show==0:
+        pygame.display.update()
+        grafika=pygame.image.load(os.path.join("MenuPic.png"))
+        screen.blit( grafika, (0, 0) )
+
 
 
