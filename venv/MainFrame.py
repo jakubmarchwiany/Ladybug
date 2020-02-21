52cd85c27882bff5d6c61bdb8a042b329c5b9ac7
Damian Chrzanowski <damianchrzanek@gmail.com>
2020-02-21 19:41:36 +0100

    tak

--- venv/MainFrame.py
+++ venv/MainFrame.py
@@ -1,13 +1,36 @@
-#Glowne Okno Aplikacji
+# Glowne Okno Aplikacji
 
 import pygame
+import random
+from Ladybug import *
 
 pygame.init()
 pygame.display.set_caption("Biedronkowa Zawierucha")
 
-height=500
-weight=500
+height = 500
+width = 500
 
-screen=pygame.display.set_mode((height,weight))
+screen = pygame.display.set_mode((height, width))
+white = (255, 255, 255)
+screen.fill(white)
 
+ladybugs = []
+
+numbers_of_ladybug = 100
+
+for i in range(1, numbers_of_ladybug):
+    randomX = random.randint(0, width - 1)
+    randomY = random.randint(0, height - 1)
+
+    ladybugs.append(Ladybug(randomX, randomY))
+
+for ladybug in ladybugs:
+    ladybug.draw(screen)
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
